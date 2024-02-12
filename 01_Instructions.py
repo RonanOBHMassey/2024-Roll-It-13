8313a95ad8a272331f90a2e9611ccfe37092a102
Ronan O'Brien-Hall <135774404+RonanOBHMassey@users.noreply.github.com>
2024-02-12 14:57:14 +1300

    Added Full Instructions, Polished Code.

--- 01_Instructions.py
+++ 01_Instructions.py
@@ -1,18 +1,52 @@
-print(" o(^▽^)o 🎲🎲 Roll it 13! 🎲🎲 o(^▽^)o ")
+# Checks users enter yes (y) and no (n)
+def yes_no(question):
+    while True:
+        response = input(question).lower()
+
+        # Checks users response, question
+        # Repeats if users don't enter yes / no
+        if response == "yes" or response == "y":
+            return "yes"
+        elif response == "no" or response == "n":
+            return "no"
+        else:
+            print("Please enter yes / no")
+
+
+def instructions():
+    print('''
+**** INSTRUCTIONS ****
+
+To begin, decide on a score goal
+(eg: the first one to reach a score of 50 wins).
+
+For each round of the game, you win points by rolling dice.
+The winner of the round is the one who gets 13 (or slightly less)
+
+If you win the round, then your score will increase by the number of points you earned.
+If your first roll of two dice is a double (eg: both dice are the same number) 
+then your score will be DOUBLE the number of points
+
+If you lose the round, say goodbye to those potential points!
+
+If you and the computer tie, (eg: you both get a score of 11) 
+then you will get the points and the computer will not.
+
+Good Luck, Player!
+    ''')
+# Main Routine
+print('''
+o(^▽^)o 🎲🎲 Roll it 13! 🎲🎲 o(^▽^)o
+''')
 
 
 # Loop for testing purposes.
 
-while True:
-    want_instructions = input("Do you want to read the instructions? (Y/N)= ").lower()
 
-    # Checks users enter yes (y) and no (n)
-    if want_instructions == "yes" or want_instructions == "y":
-        print("you chose yes")
-    elif want_instructions == "no" or want_instructions == "n":
-        print("you chose no")
-    elif want_instructions == "yesno" or want_instructions == "yn":
-        print("you thought you were clever...")
-        exit()
-    else:
-        print("You did not choose a valid response.")
+want_instructions = yes_no("Do you want to read the instructions? (Y/N)= ")
+
+# Checks users enter yes (y) and no (n)
+if want_instructions == "yes" or want_instructions == "y":
+    instructions()
+
+print("program continuing")
